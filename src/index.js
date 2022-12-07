ea27b88bd64f8638e9eb038142b17187bf047c9d
levy <lnjiru43@gmail.com>
2022-12-07 15:03:54 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  document.querySelector("form").addEventListener('submit',(e)=>{
+    e.preventDefault()
+    createTodo(e.target.children[1].value)    
+
+    
+  })
+
+
 });
+function createTodo(todo){
+  //if(todo != ""){
+    let x = document.createElement('button')
+    let lis = document.createElement("li")
+    lis.textContent = `${todo} `
+    lis.appendChild(x)
+    x.innerHTML = ' x'
+    x.addEventListener(
+      'click', () => {
+        lis.remove()
+      }) 
+
+    document.querySelector("#tasks").appendChild(lis);
+
+  //}else alert("value not found")
+
+  
+}
